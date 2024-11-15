34673ef0b066571778888a9575d8a373643f61cd
sssongbird <caldur33@gmail.com>
2024-11-15 13:25:46 +0100

    neu

--- src/main/java/org/example/Zentralbank.java
+++ src/main/java/org/example/Zentralbank.java
@@ -1,2 +1,26 @@
-package org.example;public class Zentralbank {
+package org.example;
+
+public class Zentralbank {
+    //Der Konstruktor wird als privat deklariert -> so kann nicht mehr von außerhalb auf die Klasse zugegriffen werden
+    private Zentralbank() {
+    }
+    //Es wird ein privates und statisches Objekt der Klasse erzeugt -> keine anderen Klassen haben Zugriff auf dieses Objekt
+    private static Zentralbank zentralbank = new Zentralbank();
+
+    //statische Methode über die man das Objekt zentralbank ausgibt
+    //dieses Objekt kann nur über diese Methode ausgegeben werden
+    public static Zentralbank getInstance() {
+        return zentralbank;
+    }
+
+    private double leitzins = 0.5;
+
+    public double getLeitzins() {
+        return leitzins;
+    }
+
+    public void setLeitzins(double leitzins) {
+        if (leitzins > 0.5)
+            this.leitzins = leitzins;
+    }
 }
